6e0bf3cd71a25d87991c46d6d5d63d3364aa116d
Diego <lima.j.diego@gmail.com>
2021-06-24 11:11:07 -0300

    Criado a classe account e colocado os atributos

---
+++ Account/Account.cs
@@ -0,0 +1,12 @@
+namespace WebBank
+{
+    public class Account
+    {
+        //Atributos da conta
+        private AccountType AccountType { get; set; }
+
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+        private string None { get; set; }
+    }
+}
