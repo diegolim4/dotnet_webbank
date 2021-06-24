cfa0c6241ebe57b9ebda4c432ce64e6b26df3d2c
Diego <lima.j.diego@gmail.com>
2021-06-24 10:45:56 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
