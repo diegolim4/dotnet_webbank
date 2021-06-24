71202f1d89582b8fb3e834f01570756626057f5b
Diego <lima.j.diego@gmail.com>
2021-06-24 12:34:53 -0300

    Implementando vários métodos

--- Account/Account.cs
+++ Account/Account.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace WebBank
 {
     public class Account
@@ -17,6 +19,37 @@ namespace WebBank
             this.Credit = credit;
             this.Name = name;
         }
+
+        //Método sacar
+        public bool GetMoney(double ValuesGet)
+        {
+            //validar se o saldo é suficiente
+            if (this.Balance - ValuesGet < this.Credit * -1)
+            {
+                Console.WriteLine("Saldo insuficiente!");
+                return false;
+            }
+            this.Credit -= ValuesGet;
+            Console.WriteLine("Saldo Atual da conta de {0} é {1}", this.Name, this.Balance);
+            return true;
+        }
+
+        //método depósito
+        public void Deposit(double ValuesDeposit)
+        {
+            this.Credit += ValuesDeposit;
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Name, this.Balance);
+        }
+
+        //método tranferir
+        public void Transfer(double ValuesTransfer, Account DestinationAccount)
+        {
+            if (this.GetMoney(ValuesTransfer))
+            {
+                DestinationAccount.Deposit(ValuesTransfer);
+            }
+        }
+        
 
     }
 }
