992dba4edca4baaf60a3cbf2e6387ef181f0f127
Diego <lima.j.diego@gmail.com>
2021-06-24 11:26:58 -0300

    Adicionado método construtor

--- Account/Account.cs
+++ Account/Account.cs
@@ -5,8 +5,18 @@ namespace WebBank
         //Atributos da conta
         private AccountType AccountType { get; set; }
 
-        private double Saldo { get; set; }
-        private double Credito { get; set; }
-        private string None { get; set; }
+        private double Balance { get; set; }
+        private double Credit { get; set; }
+        private string Name { get; set; }
+
+        //Método construtor        
+        public Account(AccountType accountType, double balance, double credit, string name)
+        {
+            this.AccountType = accountType;
+            this.Balance = balance;
+            this.Credit = credit;
+            this.Name = name;
+        }
+
     }
 }
